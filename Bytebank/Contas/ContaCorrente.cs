884f619317f08b211c2d131146cba7ed6d4e6537
felip <felip@FELIPPE>
2022-10-17 23:50:27 -0300

    Adicionar arquivos de projeto.

---
+++ Bytebank/Contas/ContaCorrente.cs
@@ -0,0 +1,64 @@
+﻿using Bytebank.Titular;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank.Contas
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public Cliente titular;
+        private double saldo;
+
+        public void Depositar(double Valor)
+        {
+            saldo += Valor;
+        }
+
+        public bool Sacar(double Valor)
+        {
+            if (Valor <= saldo)
+            {
+                saldo -= Valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+        }
+        public void SetSaldo(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                this.saldo = valor;
+            }
+        }
+        public double GetSaldo()
+        {
+            return this.saldo;
+        }
+    }
+}
---
+++ Bytebank/Program.cs
@@ -0,0 +1,25 @@
+﻿using Bytebank.Contas;
+using Bytebank.Titular;
+
+
+Cliente cliente = new Cliente();
+cliente.nome = "Felippe Silva";
+cliente.cpf = "123456789";
+cliente.profissao = "Developers";
+
+ContaCorrente conta = new ContaCorrente();
+conta.titular = cliente;
+conta.conta = "1010-X";
+conta.numero_agencia = 15;
+conta.SetSaldo (100);
+
+Console.WriteLine("Titular = " + conta.titular.nome);
+Console.WriteLine("Cpf = " + conta.titular.cpf);
+Console.WriteLine("Profissao = " + conta.titular.profissao);
+Console.WriteLine("Nº Conta = " + conta.conta);
+Console.WriteLine("Saldo = " + conta.GetSaldo());
+Console.WriteLine("Nº Agência = " + conta.numero_agencia);
+
+ContaCorrente conta2 = new ContaCorrente();
+conta2.SetSaldo (-10);
+Console.WriteLine(conta2.GetSaldo());
